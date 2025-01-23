b7c89f0f0ee32fa8ac4e4e41f255ba93c925c66e
minabkt <leemina216@gmail.com>
2025-01-23 05:15:08 +0000

    mv

--- Class3/sg.tf
+++ Class3/sg.tf
@@ -1,15 +1,24 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = "${aws_vpc.main.id}"
+ 
 
   ingress {
     description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = aws_vpc.main.cidr_block
+    cidr_blocks = ["0.0.0.0/0"]
   }
+    
+    ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   egress {
     from_port   = 0
@@ -22,3 +31,4 @@ resource "aws_security_group" "allow_tls" {
     Name = "allow_tls"
   }
 }
+
